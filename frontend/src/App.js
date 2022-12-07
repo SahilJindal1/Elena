cc8a616c91982550aa7cbdcd8aabcb14e19e223e
Avani18 <500060696@stu.upes.ac.in>
2022-12-07 01:03:37 -0500

    added values in displayRoute

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import InputView from './view/InputView.js';
-import MapboxView from './view/MapboxView.js';
+import {MapboxView, DisplayRoute} from './view/MapboxView.js';
 import AlgorithmTableView from './view/AlgorithmTableView.js';
 import "./App.css";
+//import  from './view/MapboxView.js';
 
 export default function App() {
   
   
   const [tableValues, setTableValues] = useState()
 
+  //const showRoute = (pathValue) => {
+    
+  //}
+
   const setAlgorithmValues = (pathValue) => {
-    setTableValues(pathValue)
+    setTableValues(pathValue);
+    DisplayRoute(pathValue);
   }
 
     return (
